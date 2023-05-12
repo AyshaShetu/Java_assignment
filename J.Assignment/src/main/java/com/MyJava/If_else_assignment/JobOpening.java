dec669d74f86b403c78607b90b4ff864a11900f7
AyshaShetu <ashetu.ctg@gmail.com>
2023-05-12 01:10:01 -0500

    trying if else assignment to push

---
+++ J.Assignment/src/main/java/com/MyJava/If_else_assignment/JobOpening.java
@@ -0,0 +1,17 @@
+package com.MyJava.If_else_assignment;
+
+public class JobOpening {
+    public static void main(String[] args){
+        String title="QA engineer";
+        String description= "Entry Level job";
+        int requirements= 10;
+        int number_of_JobOpening= 10;
+
+        if (requirements == 10) {
+            System.out.println("you are right");
+        }
+        else {
+            System.out.println("enter correct number");
+        }
+    }
+}
